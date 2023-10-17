a895f43c89abdac24f00a0498f56d72658c85c78
sava9ecode <sava9emode@yandex.ru>
2023-10-17 23:22:03 +0300

    Initial commit

---
+++ projects/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Project
+
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {"projects": projects}
+    return render(request, "projects/project_index.html", context)
+
+
+def project_detail(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    context = {"project": project}
+    return render(request, "projects/project_detail.html", context)
